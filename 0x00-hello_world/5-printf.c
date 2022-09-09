d7c41d541107f75f199b6a4d842901c013c9b3e9
user <moyakivincent@yahoo.com>
2022-09-09 03:45:27 +0100

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * Description: A proper Desc
- * Return: 0 (Success)
+ * main - Entry point
+ * Description: Short description
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
